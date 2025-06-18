59462a74f9e168c4334b340129c3984c2c7c1ffc
Murad Mammadzada <92033053+MuradMT@users.noreply.github.com>
2025-06-18 18:43:29 +0100

    Intro to spring boot

---
+++ src/test/java/org/example/learning_spring_boot/LearningSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.learning_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
